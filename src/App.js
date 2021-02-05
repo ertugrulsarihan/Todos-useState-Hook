cfb1ab70eb48dc850f8e1fe01768a35dd6c658f8
Ertuğrul Sarıhan <ertugrulsarihan1@gmail.com>
2021-02-05 21:55:07 +0300

    m

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Container } from "react-bootstrap";
+import React, { useReducer, useState } from "react";
 
-function App() {
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add_todo":
+      return {
+        
+        todos: [
+          ...state.todos,
+          { text: action.text, completed: false},
+        ],
+      };
+    case "already_do":
+      return {
+        todos: state.todos.map((todo, idx) =>
+          idx === action.idx
+            ? {
+                ...todo,
+                completed: !todo.completed,
+              }
+            : todo
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const [{ todos }, dispatch] = useReducer(reducer, { todos: [] });
+  const [text, setText] = useState();
+
+  const handlechange = (e) => {
+     setText(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="main-cont">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch({ type: "add_todo", text });
+          setText("");
+          console.log(todos);
+        }}
+      >
+        <input value={text} onChange={handlechange} />
+      </form>
+
+      <div className="main-div">
+        {todos.map((todo, idx) => (
+          <div className="main-div" onClick={() => dispatch({ type: "already_do", idx })}>
+            <h3 className={todo.completed ? "div-text" : ""}>{todo.text}</h3>
+            <button>
+              Done
+            </button>
+          </div>
+        ))}
+      </div>
+    </Container>
   );
-}
+};
 
 export default App;
